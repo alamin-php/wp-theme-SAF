7b2e69e5df40e7d87a7559fdf34c69719e3f08c8
alamin-php <alamin.php100@gmail.com>
2021-07-10 00:04:20 +0600

    added theme bootstraping

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function saf_bootstraping(){
+    load_theme_textdomain("saf");
+    add_theme_support( "title-tag" );
+}
+add_action( "after_setup_theme", "saf_bootstraping");
+
+function saf_assets() {
+    wp_enqueue_style( "bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+    wp_enqueue_style( "saf-style", get_stylesheet_uri() );
+    // wp_enqueue_script( "script-name", get_template_directory_uri() . "/js/example.js", array(), "1.0.0", true );
+}
+add_action( "wp_enqueue_scripts", "saf_assets" );
